7cf973045ef3754b9607e23803126c065cc459e5
Dawa <sherpadawa01@outlook.com>
2023-01-15 21:56:31 +0545

    formula added

---
+++ src/utils/moveCheckedLogic.ts
@@ -0,0 +1,27 @@
+export const isColumOfFour = (
+  newBoard: string[],
+  boardSize: number,
+  formulaForColumnOfFour: number
+) => {
+  for (let i: number = 0; i <= formulaForColumnOfFour; i++) {
+    const columnOfFour: number[] = [
+      i,
+      i + boardSize,
+      i + boardSize * 2,
+      i + boardSize * 3,
+    ];
+
+    const decidedColor: string = newBoard[i];
+
+    const isBlank: boolean = newBoard[i] === '';
+
+    if (
+      columnOfFour.every(
+        (candy: number) => newBoard[candy] === decidedColor && !isBlank
+      )
+    ) {
+      columnOfFour.forEach((candy: number) => (newBoard[candy] = ''));
+      return true;
+    }
+  }
+};
